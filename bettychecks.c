7b4c0e521131823853466c3d9b1bd4d67489c0a9
52311C <karanjacindy1@gmail.com>
2023-05-17 13:12:45 -0700

    Betty checks

---
+++ bettychecks.c
@@ -0,0 +1,20 @@
+#include "header.h"
+
+/**
+ * main - Code entry
+ * Description: A code that passes the betty checks
+ * Return: o
+ */
+
+int main (void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	printf("\n");
+	return (0);
+}
---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <stdarg.h>
+
+int _putchar (char c);
+
+#endif
